d410e090ac29de7e56ca9291901323d9030e3b88
Brittani Taylor <brittani.elan.taylor@gmail.com>
2020-09-10 16:27:26 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,24 @@
 # Write your methods here
+
+def loop_message_five_times(message)
+  5.times do
+    puts message
+  end
+end
+
+def loop_message_n_times(message, num)
+  num.times do
+    puts message
+  end
+end
+
+def output_array(message, num)
+end
+
+def return_string_array(array)
+  count = 0
+  while count == array.length
+    new_array << array[count].to_s
+    count += 1
+  end
+end
